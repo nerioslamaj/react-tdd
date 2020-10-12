2fd08bfbe5cda06711b795de99ccde050fa690e6
Nerios Lamaj <nerios.lamaj@gmail.com>
2020-10-12 14:24:47 +0100

    AppointmentsDayView renders a DIV with the right ID

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Appointment } from '../src/Appointment'
+import { Appointment, AppointmentsDayView } from '../src/Appointment'
 
 describe('Appointment', () => {
 
@@ -25,5 +25,22 @@ describe('Appointment', () => {
         render(<Appointment customer={ customer }/>, container)
 
         expect(container.textContent).toMatch('Jordan')
+    })
+})
+
+describe('AppointmentsDayView', () => {
+
+    let container
+
+    beforeEach(() => {
+        container = document.createElement('div')
+    })
+
+    const render = component => ReactDOM.render(component, container)
+
+    it('renders a DIV with the right ID', () => {
+        render(<AppointmentsDayView appointments={ [] }/>, container)
+
+        expect(container.querySelector('div#appointmentsDayView')).not.toBeNull()
     })
 })
